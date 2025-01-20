0472b866958ec80a7ccfda0abd46bfa4bb71652e
John <jbhumph@gmail.com>
2025-01-20 08:54:08 -0800

    Finished calculate algorithm in C and freed allocated memory.

--- triangle.c
+++ triangle.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// recursive method for calculating triangle
+void calculate(int *triangle[], int x, int y, int lines) {
+    if (x >= lines || y > x) {
+        return;
+    } else if (y == 0 || y == x) {
+        triangle[x][y] = 1;
+    } else {
+        triangle[x][y] = triangle[x-1][y-1] + triangle[x-1][y];
+    } 
+    calculate(triangle, x+1, y, lines);
+    calculate(triangle, x+1, y+1, lines);
+}
+
 // main method for program
 int main(void) {
     // get user input
@@ -9,21 +22,27 @@ int main(void) {
     scanf("%d", &lines);
 
     // declare array of pointers
-    int *triangle[lines];
+    int **triangle = malloc(lines * sizeof(int *));
     for (int i = 0; i < lines; i++) {
-        triangle[i] = malloc(i+1 * sizeof(int));
-        for (int j = 0; j < i+1; j++) {
-            triangle[i][j] = 1;
-        }
+        triangle[i] = calloc(i + 1, sizeof(int));
     }
+
+    // call method to calculate triangle
+    calculate(triangle, 0, 0, lines);
 
     // print the triangle
     for (int i = 0; i < lines; i++) {
         for (int j = 0; j < i + 1; j++) {
-            printf("%d", triangle[i][j]);
+            printf("%d  ", triangle[i][j]);
         }
         printf("\n");
-
     }
+
+    // free memory
+    for (int i = 0; i < lines; i++) {
+        free(triangle[i]);
+    }
+    free(triangle);
+
     return 0;
 }
